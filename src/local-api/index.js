58bb6ae05f40be2e8dffc010abcf5db315e6cb55
jmmascorro <jessemascorro88@gmail.com>
2022-09-29 20:28:50 -0500

    made a change to port to test jenkins

--- src/local-api/index.js
+++ src/local-api/index.js
@@ -6,7 +6,8 @@ import { sequelize } from "./db/index.js";
 import LOCAL_API from "../data/localAPI.mjs"
 
 const app = express();
-const port = process.env.PORT || LOCAL_API.PORT;
+const port = process.env.PORT || 3306;
+
 
 sequelize.sync()
 .then(result => {
