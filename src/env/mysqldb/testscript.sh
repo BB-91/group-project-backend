f8674e1e307c968159440d1289d4f41c2b7652c3
jmmascorro <jessemascorro88@gmail.com>
2022-10-05 13:24:28 -0500

    commit

--- src/env/mysqldb/testscript.sh
+++ src/env/mysqldb/testscript.sh
@@ -12,6 +12,7 @@ sudo systemctl status mysql.service
 sudo mysql -u root -ppassword -e "CREATE USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';"
 sudo mysql -u root -ppassword -e "GRANT ALL PRIVILEGES ON test_local_profiles_api.* to 'root';"
 
+sudo mysql -u root -ppassword -e "DROP DATABASE test_local_profiles_api;"
 sudo mysql -u root -ppassword -e "CREATE DATABASE test_local_profiles_api;"
 
 # sudo mysql -uroot -p$2 -e "USE local_profiles;"
